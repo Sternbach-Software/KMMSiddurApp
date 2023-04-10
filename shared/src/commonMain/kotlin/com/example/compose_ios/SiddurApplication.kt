1f789e383917c2f252a0470a9f8e0830d40c35b1
shmueldabomb441 <shmuelsternbach613@gmail.com>
2023-04-09 22:21:49 -0400

    1. Align text center 2. add padding 3. make scrollable 4. use shachris text 5. make it 18sp 6. Add topappbar to iOS 7. Replace app bar text with "Free siddur"

--- shared/src/commonMain/kotlin/com/example/compose_ios/SiddurApplication.kt
+++ shared/src/commonMain/kotlin/com/example/compose_ios/SiddurApplication.kt
@@ -1,8 +1,8 @@
 package com.example.compose_ios
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
@@ -10,21 +10,33 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.ExperimentalUnitApi
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
+import androidx.compose.ui.unit.dp
 import com.example.compose_ios.util.parseMarkdown
+import com.example.compose_ios.strings.AshkenazSiddur
 
+@OptIn(ExperimentalUnitApi::class)
 @Composable
 internal fun SiddurApplication(displayTopAppBar: Boolean) {
     MaterialTheme {
         Scaffold(
-            topBar = { if(displayTopAppBar) TopAppBar() { Text("Hello") } },
+            topBar = { /*if(displayTopAppBar) */TopAppBar() { Text("Free siddur") } },
             content = {
                 Box(
                     modifier = Modifier.fillMaxSize(),
                     contentAlignment = Alignment.Center,
                 ) {
-                    Column {
+                    Column(
+                        modifier = Modifier
+                            .verticalScroll(rememberScrollState())
+                            .padding(16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
                         Text("From Compose! SiddurApplication()")
-                        Text("What if <b>I <em>want <small>to display another one?</small></em></b>".parseMarkdown())
+                        Text(AshkenazSiddur.SHACHRIS.parseMarkdown(), fontSize = TextUnit(18F, TextUnitType.Sp))
                     }
                 }
             },
